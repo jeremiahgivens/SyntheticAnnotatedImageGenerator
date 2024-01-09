4a89e7aafe99d493fe3a4aa92505ab22251bd8dd
Jeremiah Givens <jeremiahgivens149@gmail.com>
2024-01-09 17:35:09 -0600

    Fixed issue that was preventing press of "A" button to be reliably captured.

--- Assets/Scripts/CameraMovement/XboxControllerCameraController.cs
+++ Assets/Scripts/CameraMovement/XboxControllerCameraController.cs
@@ -1,3 +1,4 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,9 +8,10 @@ public class XboxControllerCameraController : MonoBehaviour
     public float m_YSpeed = 1f;
     public float m_AngularSpeed = 90f;
     public bool m_IsConstrained = false;
+    public float m_ClampingHeight = 2;
 
     private float m_CameraPanAngle = 0f;
-    void FixedUpdate()
+    void Update()
     {
         ProcessInput();
     }
@@ -26,22 +28,70 @@ public class XboxControllerCameraController : MonoBehaviour
         movementInput.leftTrigger = gamepad.leftTrigger.value;
         movementInput.rightTrigger = gamepad.rightTrigger.value;
         movementInput.buttonA = gamepad.aButton.wasPressedThisFrame;
+
+        if (movementInput.buttonA)
+        {
+            m_IsConstrained = !m_IsConstrained;
+        } else if (m_IsConstrained)
+        {
+            HandleConstrainedMovement(movementInput);
+        }
+        else
+        {
+            HandleFreeMovement(movementInput);
+        }
         
-        HandleFreeMovement(movementInput);
+        
     }
 
     private void HandleConstrainedMovement(MovementInput movementInput)
     {
         // Method to be called in FixedUpdate() when in constrained mode (clamped to surface)
+        float delta = Time.deltaTime;
+        Vector3 pos = transform.position + transform.forward * movementInput.leftStick.y * m_XZSpeed * delta +
+                      transform.right * movementInput.leftStick.x * m_XZSpeed * delta;
+        m_ClampingHeight += (movementInput.rightTrigger - movementInput.leftTrigger) * m_YSpeed * delta;
+        m_ClampingHeight = Mathf.Max(m_ClampingHeight, 0.5f);
+
+        ClampCamera(pos);
+
+        float panAngleDelta = movementInput.rightStick.y * m_AngularSpeed * delta;
+        m_CameraPanAngle += panAngleDelta;
+        if (m_CameraPanAngle < -90)
+        {
+            panAngleDelta += 90 - m_CameraPanAngle;
+            m_CameraPanAngle = -90;
+        } else if (m_CameraPanAngle > 90)
+        {
+            panAngleDelta -= m_CameraPanAngle - 90;
+            m_CameraPanAngle = 90;
+        }
+        
+        transform.Rotate(Vector3.up, movementInput.rightStick.x * m_AngularSpeed * delta, relativeTo: Space.World);
+        transform.Rotate(transform.right, -panAngleDelta, relativeTo: Space.World);
+    }
+
+    private void ClampCamera(Vector3 pos)
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(pos + Vector3.up * 10000, Vector3.down, out hit, Mathf.Infinity))
+        {
+            transform.position = hit.point + Vector3.up * m_ClampingHeight;
+        }
+        else
+        {
+            m_IsConstrained = false;
+        }
     }
 
     private void HandleFreeMovement(MovementInput movementInput)
     {
         // Method to be called in FixedUpdate() when in free mode (four degrees of freedom)
         float delta = Time.deltaTime;
-        Vector3 pos = transform.position + transform.forward * movementInput.leftStick.y * m_XZSpeed * delta + transform.right *
-            movementInput.leftStick.x * m_XZSpeed * delta + (movementInput.rightTrigger - movementInput.leftTrigger)
-            * m_YSpeed * transform.up * delta;
+        Vector3 pos = transform.position + transform.forward * movementInput.leftStick.y * m_XZSpeed * delta +
+                      transform.right * movementInput.leftStick.x * m_XZSpeed * delta +
+                      (movementInput.rightTrigger - movementInput.leftTrigger) * m_YSpeed * transform.up * delta;
 
         transform.position = pos;
 
