62b4ac7715a0f9fe31a70a325e76d75794921559
Jeremiah Givens <jeremiahgivens149@gmail.com>
2024-01-09 15:54:37 -0600

    Added script for controlling camera with xbox controller.

---
+++ Assets/Scripts/CameraMovement/XboxControllerCameraController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class XboxControllerCameraController : MonoBehaviour
+{
+    public float m_XZSpeed = 100f;
+    public float m_YSpeed = 1f;
+    public float m_AngularSpeed = 90f;
+    public bool m_IsConstrained = false;
+
+    private float m_CameraPanAngle = 0f;
+    void FixedUpdate()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        var gamepad = Gamepad.current;
+        if (gamepad == null)
+            return;
+
+        MovementInput movementInput = new MovementInput();
+        movementInput.leftStick = gamepad.leftStick.ReadValue();
+        movementInput.rightStick = gamepad.rightStick.ReadValue();
+        movementInput.leftTrigger = gamepad.leftTrigger.value;
+        movementInput.rightTrigger = gamepad.rightTrigger.value;
+        movementInput.buttonA = gamepad.aButton.wasPressedThisFrame;
+        
+        HandleFreeMovement(movementInput);
+    }
+
+    private void HandleConstrainedMovement(MovementInput movementInput)
+    {
+        // Method to be called in FixedUpdate() when in constrained mode (clamped to surface)
+    }
+
+    private void HandleFreeMovement(MovementInput movementInput)
+    {
+        // Method to be called in FixedUpdate() when in free mode (four degrees of freedom)
+        float delta = Time.deltaTime;
+        Vector3 pos = transform.position + transform.forward * movementInput.leftStick.y * m_XZSpeed + transform.right *
+            movementInput.leftStick.x * m_XZSpeed + (movementInput.rightTrigger - movementInput.leftTrigger)* m_YSpeed * transform.up;
+
+        transform.position = pos;
+
+        float panAngleDelta = movementInput.rightStick.y * m_AngularSpeed;
+        m_CameraPanAngle += panAngleDelta;
+        if (m_CameraPanAngle < -90)
+        {
+            panAngleDelta += 90 - m_CameraPanAngle;
+            m_CameraPanAngle = -90;
+        } else if (m_CameraPanAngle > 90)
+        {
+            panAngleDelta -= m_CameraPanAngle - 90;
+            m_CameraPanAngle = 90;
+        }
+        
+        transform.Rotate(Vector3.up, -movementInput.rightStick.x * m_AngularSpeed);
+        transform.Rotate(transform.right, panAngleDelta);
+
+        Debug.Log("Right trigger: " + movementInput.rightTrigger + ", left stick.y: " + movementInput.leftStick.y);
+    }
+    
+    public struct MovementInput
+    {
+        // Analog
+        public Vector2 leftStick;
+        public Vector2 rightStick;
+
+        // Buttons
+        public bool buttonA;
+        public float rightTrigger;
+        public float leftTrigger;
+    }
+}
